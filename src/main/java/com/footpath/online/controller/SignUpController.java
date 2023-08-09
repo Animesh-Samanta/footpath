2d38728051a9fbf3f69e34ab778f2cd9c876414b
Somnath Metya <somnathmetya57@gmail.com>
2023-08-09 16:39:09 +0530

    Noon commit Signup Id card

--- src/main/java/com/footpath/online/controller/SignUpController.java
+++ src/main/java/com/footpath/online/controller/SignUpController.java
@@ -2,6 +2,8 @@ package com.footpath.online.controller;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.footpath.online.RequiredAnnotation;
 import com.footpath.online.model.Customer;
@@ -28,6 +32,16 @@ public class SignUpController {
 	
 @PostMapping(value="/signup")
 public ResponseEntity<?> signUp(@Valid @RequestBody Customer customer) {
+	if(signUpService.insertNewUser(customer)) {
+	return new ResponseEntity<>(requiredA.messageSource.getMessage("USER_CREATED", null, null, null), HttpStatus.CREATED);
+	}
+	else {
+		return new ResponseEntity<>(requiredA.messageSource.getMessage("USER_EXIST", null, null, null), HttpStatus.NOT_ACCEPTABLE);
+	}
+	
+}
+@PostMapping(value="/signupidcard")
+public ResponseEntity<?> signUpUsingIdCard(@Valid @RequestPart("customer") Customer customer,@RequestPart("file") List<MultipartFile> files) {
 	if(signUpService.insertNewUser(customer)) {
 	return new ResponseEntity<>(requiredA.messageSource.getMessage("USER_CREATED", null, null, null), HttpStatus.CREATED);
 	}
